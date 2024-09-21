0ce7349d3055999300adce83092a6eab605ffa6f
whizthom <107649124+whizthom@users.noreply.github.com>
2024-09-21 12:59:32 +0100

    Add files via upload

---
+++ Dicee Challenge - Completed/index.js
@@ -0,0 +1,22 @@
+
+var randomNumber1 = (Math.floor(Math.random() * 6)) + 1;
+var randomImage1 = "images/dice" + randomNumber1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomImage1);
+
+
+var randomNumber2 = (Math.floor(Math.random() * 6)) + 1;
+var randomImage2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImage2);
+
+
+if (randomImage1 > randomImage2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}
+
+else if (randomImage2 > randomImage1) {
+  document.querySelector("h1").innerHTML = "Play 2 Wins 🚩 !"
+}
+
+else (
+  document.querySelector("h1").innerHTML = "Draw"
+)
